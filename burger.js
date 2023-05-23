da49cb9a7c665df0108d13fe31c3926f3590d22a
slowrunr <mungaslov.nft@gmail.com>
2023-05-23 23:57:16 +0900

    workin on burger-btn.undone

--- burger.js
+++ burger.js
@@ -1,24 +1,30 @@
-const BURGER_OPENED_CLASSNAME = 'burger_open';
-const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
-const BODY_FIXED_CLASSNAME = 'body_fixed';
+const BURGER_OPENED_CLASSNAME = "burger_open";
+const BURGER_BTN_OPENED_CLASSNAME = "burger-btn_open";
+const BODY_FIXED_CLASSNAME = "body_fixed";
 
-const bodyNode = document.querySelector('body');
-const burgerNode = document.querySelector('.js-burger');
-const burgerBtnNode = document.querySelector('.js-burger-btn');
-const burgerContentNode = document.querySelector('.js-burger__content')
+const bodyNode = document.querySelector("body");
+const burgerNode = document.querySelector(".js-burger");
+const burgerBtnNode = document.querySelector(".js-burger-btn");
+const burgerContentNode = document.querySelector(".js-burger__content");
 
-burgerBtnNode.addEventListener('click', toggleBurger);
+burgerBtnNode.addEventListener("click", toggleBurger);
 
-burgerNode.addEventListener('click', (event) => {
-    const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
+burgerNode.addEventListener("click", (event) => {
+  const isClickOutsideContent = !event
+    .composedPath()
+    .includes(burgerContentNode);
 
-    if (isClickOutsideContent) {
-        toggleBurger();
-    }
-})
+  if (isClickOutsideContent) {
+    toggleBurger();
+  }
+});
 
 function toggleBurger() {
-    burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
-    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
-    burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+  burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
+  bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+  burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+}
+
+function myFunction(x) {
+  x.classList.toggle("change");
 }
